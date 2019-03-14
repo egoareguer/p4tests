c865d18db4c5d5e80dec4631a4fbf4b5a8b1ae24
egoareguer <egoareguer@gmail.com>
2019-03-14 08:22:16 +0000

    Const masks fix

--- loglog/table_fill.sh
+++ loglog/table_fill.sh
@@ -3,7 +3,7 @@ rm table_zeroes_lpm
 
 for i in {1..56}; 
 do
-	echo -n "(56w0b" >> table_zeroes_lpm
+	echo -n "56w0b" >> table_zeroes_lpm
 
 	for ((j=2; j<$i+1;j++)); 
 	do 
@@ -17,5 +17,16 @@ do
 		echo -n "0" >> table_zeroes_lpm
 	done
 
-	echo "/$i) : save_zeroes($i);" >> table_zeroes_lpm
+	echo -n " &&& 56w0b" >> table_zeroes_lpm
+
+	for ((j=1; j<$i+1; j++));
+	do
+		echo -n "1" >> table_zeroes_lpm
+	done
+	for ((j=$i; j<56; j++));
+	do
+		echo -n "0" >> table_zeroes_lpm
+	done
+
+	echo " : save_zeroes($i);" >> table_zeroes_lpm
 done
