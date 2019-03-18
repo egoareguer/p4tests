0132ad23fcf127b6cf7e195d8577ccdf3fcef223
egoareguer <egoareguer@gmail.com>
2019-03-18 14:44:04 +0000

    Fixed prefix/mask log writes

--- arithmetic/table_fill.sh
+++ arithmetic/table_fill.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 rm log_table.entries
 
-: ' We want to write the log table entries. There is 2^m of the form 
+: ' We want to write the log/exp table entries. There is ~ N*2^m of the form 
  
 	0^n ++ 1(0|1)^min-m-1,N-n-1) ++ *^max(0,N-n-m) for 0 <= n < N
 	
-	* Corresponds to "whatever", so it needs a mask of its length assorted, and a log of the assorted value written behind
+	for the log table, mapping to a L-long bit floating int with a fixed decimal point that corresponds to the average of the logs for said entry
+
+	The exp table simply maps back said value to an integer.
+	
+	The * character corresponds to "whatever", so it needs a mask of its length assorted, and a log of the assorted value written behind
 '
 m=5
 l=32
 
-for n in {0..63}; 
+for n in {0..62}; #62 because the last line is an ugly hack 
 do
 	#Constants declaration necessary for the nested loop	
 	if [[ $((m -1)) -lt $((64 - n - 1)) ]]; 
@@ -34,24 +38,39 @@ do
 		# ----- Address start -----
 		#Prefix
 		echo -n "(64w0b" >>  log_table.entries
-		for ((j=0; j<$n;j++)); 
+		for ((j=0; j<$n;j++));  #n chars
 		do 
 			echo -n "0" >> log_table.entries
 		done
 
-		echo -n "1" >> log_table.entries
+		#echo -n " " >> log_table.entries
+		echo -n "1" >> log_table.entries #n+1 chars
 		
 		#Sliding precision window step
-		echo -n `bc <<< "obase=2; $i "` >> log_table.entries
+		#echo -n `bc <<< "obase=2; $i "` >> log_table.entries
+		var=`bc <<< "obase=2; $i"` 		
+		bc_length=`wc -m <<< $var` #True, because there's a '1' prefixing var, but bc appends a linebreak to the value
+		padding=$(($min - 1 - $bc_length))
+		
+	#	echo -n " " >> log_table.entries
+		for ((l=0;l<padding;l++)) 
+		do	
+			echo -n "0" >> log_table.entries 
+		done
+		echo -n $var >> log_table.entries #n +6 chars
 
 		#"Dont care" fill in for proper filling after the window
-		#Didn't really find a way to make bc cough up how many chars it wrote, need to read it 
+		#Didn't really find a way to make bc cough up how many chars it wrote, need to check the man
 		char_count=`tail -n1 log_table.entries | wc -m`
-		#The characters are: the prefix, (64w0b
-		remaining=$((72 - $char_count ))
-		bc_length=$((72 - $remaining - 7 - $n))
 
-		for ((k=0;k<remaining;k++))
+		#The characters are: the prefix, (64w0b
+		remaining=$((70 - $char_count ))
+	
+		#space after precision window
+		#echo -n " " >> log_table.entries
+
+	#	echo -n " " >> log_table.entries
+		for ((k=0;k<$remaining;k++))
 		do	
 			echo -n "0" >> log_table.entries
 		done
@@ -60,16 +79,40 @@ do
 		echo -n " &&& 64w0b" >> log_table.entries
 		
 		#What's must use the prefix set up
-		for ((j=0;j<$n+$bc_length+1;j++));
+		for ((j=0;j<$n+5 & j<64 ;j++));
 		do
 			echo -n "1" >> log_table.entries
 		done
 		#Zeroes to bring the mask to its proper length
-		for ((j=0;j<$remaining;j++));
+		for ((j=0;j<59-$n;j++));
 		do
 			echo -n "0" >> log_table.entries
 		done
 
-		echo "/$i) : save_zeroes($i); " >> log_table.entries
+		#representative var calculation
+		# let a = 0^n ++ 1 ++ (0|1)^min ++ 0^max
+		# if n+1+min < 64, then avg = a + 1++0^(max-1)
+		median=`tail -n1 log_table.entries | cut -d" " -f1 | cut -d"b" -f2 | cut -c "$((n+1))-"`
+		echo median $median
+		complement=`bc <<< "obase=2 ;2^$(($max -1)) "`
+		echo complement $complement
+		arg=`bc -l <<< "ibase=2; obase=2; l ($((median+complement)))"`
+		echo arg $arg
+
+		echo " : save_zeroes($arg); " >> log_table.entries
+		
+		echo "var = $var, n=$n, bc_length = $bc_length, remaining = $remaining, padding = $padding, min = $min" >> log_table.entries
 	done
 done
+
+echo -n "(64w0b" >> log_table.entries
+for n in {0..62}
+do
+	echo -n "0" >> log_table.entries 
+done
+echo -n "1 &&& 64w0b" >> log_table.entries 
+for n in {0..63}
+do
+	echo -n "1" >> log_table.entries
+done
+echo -n " : op (1);" >> log_table.entries
